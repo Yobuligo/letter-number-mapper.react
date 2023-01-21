5c56b6cbbe3a21be42a0631caf7d8d763b9fbcbc
Peter <hoffmannpeter82@gmail.com>
2023-01-21 21:50:06 +0100

    Introduce IToolbarAction

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../AppContext";
+import settingsImage from "../../images/settings.png";
 import { SymbolMapper } from "../../symbolInfo/SymbolMapper";
 import { Display } from "../display/Display";
 import Keyboard from "../keyboard/Keyboard";
 import Settings from "../settings/Settings";
+import IToolbarAction from "../toolbar/IToolbarAction";
 import Toolbar from "../toolbar/Toolbar";
 import styles from "./Main.module.css";
 
@@ -16,9 +18,19 @@ export const Main: React.FC<{
     console.log(SymbolMapper.numberToLetter(selectedSymbol));
   };
 
+  const toolbarActions: IToolbarAction[] = [
+    {
+      source: settingsImage,
+      text: "Settings",
+      onClickHandler: () => {
+        console.log(`Settings button was clicked`);
+      },
+    },
+  ];
+
   return (
     <div className={styles.main}>
-      <Toolbar />
+      <Toolbar toolbarActions={toolbarActions} />
       <Display
         symbol={props.symbol}
         exerciseType={context.settings.exerciseType}
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,12 +1,19 @@
-import settingsImage from "../../images/settings.png";
+import IToolbarAction from "./IToolbarAction";
 import styles from "./Toolbar.module.css";
 
-const Toolbar: React.FC = () => {
-  return (
-    <div className={styles.toolbar}>
-      <img src={settingsImage} alt="settings" />
-    </div>
-  );
+const Toolbar: React.FC<{ toolbarActions?: IToolbarAction[] }> = (props) => {
+  const items = props.toolbarActions?.map((toolbarAction) => {
+    return (
+      <button key={toolbarAction.source} onClick={toolbarAction.onClickHandler}>
+        <img
+          key={toolbarAction.source}
+          src={toolbarAction.source}
+          alt={toolbarAction.text}
+        />
+      </button>
+    );
+  });
+  return <div className={styles.toolbar}>{items}</div>;
 };
 
 export default Toolbar;
