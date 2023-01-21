e6fe75c98ba652c1f9ad986cf664719410e1c767
Peter <hoffmannpeter82@gmail.com>
2023-01-21 21:52:15 +0100

    Introduce Card component

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import styles from "./Card.module.css";
+
+const Card: React.FC<{ children: ReactNode }> = (props) => {
+  return <div className={styles.card}>{props.children}</div>;
+};
+
+export default Card;
