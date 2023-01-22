39bb7b312619f0895ec19bb2e9bbac965e54dbc9
Peter <hoffmannpeter82@gmail.com>
2023-01-22 22:14:47 +0100

    Reposition settings

--- src/components/core/modalDialog/ModalDialog.tsx
+++ src/components/core/modalDialog/ModalDialog.tsx
@@ -9,15 +9,7 @@ const ModalDialog: React.FC<{
   return (
     <>
       <div className={styles.backdrop} onClick={props.onConfirm}></div>
-      <div
-        className={
-          props.className !== undefined
-            ? `${props.className} ${styles.modalDialog}`
-            : styles.modalDialog
-        }
-      >
-        {props.children}
-      </div>
+      <div className={styles.modalDialog}>{props.children}</div>
     </>
   );
 };
