a562dc5c830dd00b9dbfbedda36d548b609e08ab
Peter <hoffmannpeter82@gmail.com>
2023-01-24 22:52:58 +0100

    Fix bug in TrainingProgram

--- src/training/model/TrainingProgramInfo.ts
+++ src/training/model/TrainingProgramInfo.ts
@@ -3,4 +3,8 @@ import { ITrainingProgramInfo } from "./ITrainingProgramInfo";
 
 export class TrainingProgramInfo implements ITrainingProgramInfo {
   constructor(private trainingProgram: ITrainingProgram) {}
+
+  public get probabilityWeightSum(): number {
+    throw new Error();
+  }
 }
