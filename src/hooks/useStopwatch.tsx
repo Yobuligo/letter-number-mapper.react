b08043147cabb659733c6afbc28a02f09a9c8481
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-09 21:05:55 +0100

    Implement stopwatch

--- src/hooks/useStopwatch.tsx
+++ src/hooks/useStopwatch.tsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
 
 const useStopwatch = () => {
-  const [startTime, setStartTime] = useState(new Date());
-  const [endTime, setEndTime] = useState<Date>();
+  const [startTime, setStartTime] = useState<Date>();
+  const [stopTime, setStopTime] = useState<Date>();
+  return {
+    start: () => {
+      setStartTime(new Date());
+    },
+    stop: () => {
+      setStopTime(new Date());
+    },
+    getElapsed: (): number => {
+      if (startTime === undefined) {
+        return 0;
+      }
+      const date = stopTime ?? new Date();
+      return date.getTime() - startTime.getTime();
+    },
+    reset: () => {
+      setStartTime(undefined);
+      setStopTime(undefined);
+    },
+  };
 };
 
 export default useStopwatch;
