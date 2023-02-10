7e9aecbbdf6e6d3224d841fbae8586f819558b6a
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-10 08:35:12 +0100

    Introduce IAppContext

--- src/AppContext.ts
+++ src/AppContext.ts
@@ -3,7 +3,7 @@ import { ExerciseType } from "./components/exercise/ExerciseType";
 import { SolutionStatus } from "./components/exercise/SolutionStatus";
 import { KeyboardType } from "./components/keyboard/KeyboardType";
 import { FeedbackTime } from "./components/settings/FeedbackTime";
-import { IStopwatch } from "./services/IStopwatch";
+import { IAppContext } from "./IAppContext";
 import { Stopwatch } from "./services/Stopwatch";
 
 export const STORED_PARAMETERS = "StoredParameters";
@@ -13,23 +13,7 @@ export type StoredParameters = {
   showSolvingTimeList: boolean;
 };
 
-export const AppContext = createContext<{
-  settings: {
-    storedParameters: StoredParameters;
-    setExerciseType: (exerciseType: ExerciseType) => void;
-    setFeedbackTime: (feedbackTime: FeedbackTime) => void;
-    setShowSolvingTimeList: (showSolvingTimeList: boolean) => void;
-    keyboardType: KeyboardType;
-    correctSolution?: Boolean;
-  };
-  exercise: {
-    symbol: string;
-    provideExerciseSolution: (selectedSymbol: string) => void;
-    solutionStatus: SolutionStatus;
-    solvingTimes: number[];
-  };
-  stopwatch: IStopwatch;
-}>({
+export const AppContext = createContext<IAppContext>({
   settings: {
     storedParameters: {
       exerciseType: ExerciseType.LETTER_TO_NUMBER,
---
+++ src/IAppContext.ts
@@ -0,0 +1,24 @@
+import { StoredParameters } from "./AppContext";
+import { ExerciseType } from "./components/exercise/ExerciseType";
+import { SolutionStatus } from "./components/exercise/SolutionStatus";
+import { KeyboardType } from "./components/keyboard/KeyboardType";
+import { FeedbackTime } from "./components/settings/FeedbackTime";
+import { IStopwatch } from "./services/IStopwatch";
+
+export type IAppContext = {
+  settings: {
+    storedParameters: StoredParameters;
+    setExerciseType: (exerciseType: ExerciseType) => void;
+    setFeedbackTime: (feedbackTime: FeedbackTime) => void;
+    setShowSolvingTimeList: (showSolvingTimeList: boolean) => void;
+    keyboardType: KeyboardType;
+    correctSolution?: Boolean;
+  };
+  exercise: {
+    symbol: string;
+    provideExerciseSolution: (selectedSymbol: string) => void;
+    solutionStatus: SolutionStatus;
+    solvingTimes: number[];
+  };
+  stopwatch: IStopwatch;
+};
