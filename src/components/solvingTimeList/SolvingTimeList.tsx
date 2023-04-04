5a7a72a436d464ec5d847cd1575ff08e4e7fdf4c
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-04 11:52:20 +0200

    Bugfix: Rerender solving time list only if a new entry was added or deleted

--- src/components/solvingTimeList/SolvingTimeList.tsx
+++ src/components/solvingTimeList/SolvingTimeList.tsx
@@ -1,13 +1,18 @@
-import React, { useContext } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 import { AppContext } from "../../AppContext";
 import SolvingTime from "./SolvingTime";
 import styles from "./SolvingTimeList.module.css";
 
 const SolvingTimeList: React.FC = () => {
   const context = useContext(AppContext);
-  const items = context.exercise.solvingTimes.map((solvingTime) => {
-    return <SolvingTime key={crypto.randomUUID()} solvingTime={solvingTime} />;
-  });
+  const [items, setItems] = useState<ReactNode>();
+  useEffect(() => {
+    setItems(() =>
+      context.exercise.solvingTimes.map((solvingTime) => (
+        <SolvingTime key={crypto.randomUUID()} solvingTime={solvingTime} />
+      ))
+    );
+  }, [context.exercise.solvingTimes, context.exercise.solvingTimes.length]);
 
   return (
     <div className={styles.solvingTimeList}>
