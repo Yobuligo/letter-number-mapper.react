362ffa68474b0fd2d5e9fc1c91482ccf8ad2ab8a
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-17 12:07:43 +0200

    Display solving time permanently after solving symbol

--- src/components/solvingTime/SolvingTime.tsx
+++ src/components/solvingTime/SolvingTime.tsx
@@ -1,18 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../AppContext";
 import { SolutionStatus } from "../exercise/SolutionStatus";
 import styles from "./SolvingTime.module.css";
 
 const SolvingTime: React.FC = () => {
+  const [solvingTime, setSolvingTime] = useState(0);
   const context = useContext(AppContext);
+  useEffect(() => {
+    if (context.exercise.solutionStatus === SolutionStatus.SUCCESSFUL) {
+      setSolvingTime(context.stopwatch.elapsed);
+    }
+  }, [context.exercise.solutionStatus, context.stopwatch.elapsed]);
+
   return (
     <div className={styles.solvingTime}>
       {context.settings.storedParameters.showSolvingTime && (
-        <div>
-          {context.exercise.solutionStatus === SolutionStatus.SUCCESSFUL
-            ? `${context.stopwatch.elapsed} s`
-            : ""}
-        </div>
+        <div>{`${solvingTime} s`}</div>
       )}
     </div>
   );
