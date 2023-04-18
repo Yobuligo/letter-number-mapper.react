6d3a99c31afad9cb32a2546f19d136341ecc15d8
acoria <tewes.verena@gmail.com>
2023-04-18 22:08:43 +0200

    Fix invisible settings button

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -1,27 +1,27 @@
-
 import { HighlightStatus } from "./HighlightStatus";
 import { IKeyboardContext } from "./IKeyboardContext";
-import styles from './Key.module.css';
+import styles from "./Key.module.css";
 
 const Key: React.FC<{
   symbol: string;
-  highlightStatus: HighlightStatus
+  highlightStatus: HighlightStatus;
   keyboardContext: IKeyboardContext;
 }> = (props) => {
   const getClassName = () => {
     switch (props.highlightStatus) {
       case HighlightStatus.Success: {
-        return `${styles.highlight} ${styles.success}`
+        return `${styles.highlight} ${styles.success}`;
       }
       case HighlightStatus.Failed: {
-        return `${styles.highlight} ${styles.failed}`
+        return `${styles.highlight} ${styles.failed}`;
       }
-      default: return ""
+      default:
+        return "";
     }
-  }
+  };
   return (
     <button
-      className={getClassName()}
+      className={`${getClassName()} ${styles.key}`}
       onClick={() => {
         props.keyboardContext.clickHandler?.(props.symbol);
       }}
